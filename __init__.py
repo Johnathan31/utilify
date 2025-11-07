a8d0f0451f497eb2b80f016eaffc76bab131470d
Johnathan31 <ay4718636@gmail.com>
2025-11-07 20:26:11 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,3 +2,4 @@ from utilify.utilify.Variable import variable
 from utilify.utilify.Strings import strings
 from utilify.utilify.core import *
 from utilify.utilify.decorators import *
+from utilify.documentation import *
