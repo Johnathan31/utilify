1bc61a47599cf321c48ad0a2857b9267aa8c3f58
Johnathan31 <ay4718636@gmail.com>
2025-11-12 16:51:25 +0200

    Corrected the name of the function "encrypted"

--- utilify/__init__.py
+++ utilify/__init__.py
@@ -1,5 +1,5 @@
 # utilify initializer
 from Strings import strings
 from Variable import variable
-from decorators import timer, encrypter, run_once, delayed
+from decorators import timer, encrypted, run_once, delayed
 from core import *
