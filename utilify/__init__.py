fb8fa86fc7336185ffc7f66ebb1333aec51c916b
Johnathan31 <ay4718636@gmail.com>
2025-11-12 21:55:30 +0200

    Update __init__.py

--- utilify/__init__.py
+++ utilify/__init__.py
@@ -1,5 +1,2 @@
-# utilify initializer
-from Strings import strings
-from Variable import variable
-from decorators import timer, encrypted, run_once, delayed
-from core import *
+
+# utilify codes initializer
